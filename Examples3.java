29915015cba7a226b35f294e1befff743adc4010
mohammadreza1840 <54613469+mohammadreza1840@users.noreply.github.com>
2022-05-08 22:02:01 +0430

    برنامه ای بنویسید که میزان مصرف ماشین در صد کیلومتر را و گنجایش باک را دریافت کرده و میزان مسافت قابل پیمایش را نمایش دهد

---
+++ Examples3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Examples {
+    public static void main(String[] args) {
+        //Input => گنجایش باک ماشین - مصرف صد کیلومتر به لیتر
+        //Output => میزان مسافت قابل طی کردن با یک باک
+        float gonjayesh = 0, masraf = 0;
+        float masafat = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("لطفا میزان مصرف صد کیلومتر به لیتر را وارد کنید:");
+        masraf = input.nextInt();
+        System.out.print("لطفا گنجایش باک را وارد کنید:");
+        gonjayesh = input.nextInt();
+        masafat = (gonjayesh / masraf) * 100;
+        System.out.println("میزان مسافت قابل پیمایش با باک پر ماشین شما:" + masafat);
+    }
+}
