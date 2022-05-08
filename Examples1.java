b2374e7dc1427508cabbb458d13910baeba0a845
mohammadreza1840 <54613469+mohammadreza1840@users.noreply.github.com>
2022-05-08 21:45:10 +0430

    Create Examples1.java

---
+++ Examples1.java
@@ -0,0 +1,8 @@
+public class Examples1 {
+    public static void main(String[] args){
+        //      *              *
+        //  *       *     *       *
+        //*             *           *
+        System.out.println("\t\t*\t\t\t\t*\n\t*\t\t*\t\t*\t\t*\n*\t\t\t\t*\t\t\t\t*");
+    }
+}
